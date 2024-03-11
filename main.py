ec22d6be035dc10665ab1d65a881f41875f0e669
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-03-11 12:19:18 +0100

    test

--- main.py
+++ main.py
@@ -10,7 +10,3 @@ dcontroller = DestinationsController.DestinationsController(db)
 dcontroller.add_new_destination("test")
 dcontroller.add_new_destination("test2")
 dcontroller.load_available_destinations()
-car1 = Car.Car(brand='alfa', model='147', year=2006, engine_vol=1.6, engine_code='12345', power=120, body='fat', doors=4, paint_color='grey', vin='123456', user_id='testid', equipment=['test', 'test2'])
-
-# ccontroller = CarController.CarController(db)
-# ccontroller.send_car(car1, 'Żory')

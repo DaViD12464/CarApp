8d9ed2c73808037c59ddcc559a63295460c87512
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-03-14 20:05:54 +0100

    added endpoints to ensure CRUD

--- CarApi.py
+++ CarApi.py
@@ -4,8 +4,7 @@ from pymongo.server_api import ServerApi
 from baseclass.Car import *
 import os
 from dotenv import find_dotenv,load_dotenv
-
-from baseclass.Car import Car
+from bson import ObjectId
 
 '''setting up the dotenv environment'''
 #auto find the dotenv file
@@ -33,14 +32,52 @@ collection_Cars = db['Cars']
 @app.route('/')
 def index():
     return 'Welcome to CarApi!'
+
 @app.route('/all_cars', methods=['GET'])
 def get_all_cars():
     cars = list(collection_Cars.find())
     for car in cars:
         car['_id'] = str(car['_id'])
     return jsonify(cars)
+
+#Endpoint to get specific car
+@app.route('/car/<car_id>', methods=['GET'])
+def get_car(car_id):
+    car = collection_Cars.find_one({'_id': ObjectId(car_id)})
+    if car:
+        car['_id'] = str(car['_id'])
+        return jsonify(car)
+    else:
+        return jsonify({'error': 'Car not found'}), 404
+
 @app.route('/addcar', methods=['POST'])
 def add_car():
+    car_data = request.get_json()
+    if car_data:
+        result = collection_Cars.insert_one(car_data)
+        if result.inserted_id:
+            return jsonify({'message': 'Car added successfully', 'car_id': str(result.inserted_id)}), 201
+        else:
+            return jsonify({'error': 'Failed to add car'}), 500
+    else:
+        return jsonify({'error': 'No car data provided'}), 400
+
+
+@app.route('/car/<car_id>', methods=['PUT'])
+def update_car(car_id):
     data = request.get_json()
-    car = Car.model.add_car(data)
-    return jsonify(car), 201
+    updated_car = collection_Cars.update_one({'_id': ObjectId(car_id)}, {'$set': data})
+    if updated_car.modified_count:
+        return jsonify({'message': 'Car updated successfully'})
+    else:
+        return jsonify({'error': 'Car not found'}), 404
+
+
+@app.route('/car/<car_id>', methods=['DELETE'])
+def delete_car(car_id):
+    delete_result = collection_Cars.delete_one({'_id': ObjectId(car_id)})
+    if delete_result.deleted_count:
+        return jsonify({'message': 'Car deleted successfully'})
+    else:
+        return jsonify({'error': 'Car not found'}), 404
+    
--- main.py
+++ main.py
@@ -3,21 +3,17 @@ from CarApi import *
 import requests
 from baseclass.Car import *
 
-from baseclass import *
-
 app.run(host='127.0.0.1',port='8000')
-
-
 #temp
-print("Destination tests")
-dcontroller = DestinationsController.DestinationsController(db)
-dcontroller.add_new_destination("test")
-dcontroller.add_new_destination("test2")
-dcontroller.load_available_destinations()
-car1 = Car.Car(brand='alfa', model='147', year=2006, engine_vol=1.6, engine_code='12345', power=120, body='fat', doors=4, paint_color='grey', vin='123456', user_id='testid', equipment=['test', 'test2'])
+# print("Destination tests")
+# dcontroller = DestinationsController.DestinationsController(db)
+# dcontroller.add_new_destination("test")
+# dcontroller.add_new_destination("test2")
+# dcontroller.load_available_destinations()
+# car1 = Car.Car(brand='alfa', model='147', year=2006, engine_vol=1.6, engine_code='12345', power=120, body='fat', doors=4, paint_color='grey', vin='123456', user_id='testid', equipment=['test', 'test2'])
 
-ccontroller = CarController.CarController(db)
-ccontroller.send_car(car1, 'Żory')
+# ccontroller = CarController.CarController(db)
+# ccontroller.send_car(car1, 'Żory')
 
 print('adding car now\n\n\n')
 url = 'http://127.0.0.1:8000/addcar'
@@ -39,5 +35,3 @@ car_data = {
 response = requests.post(url, json=car_data)
 print(response.json())
 
-
-app.run(host='127.0.0.1',port='8000')

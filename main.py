fc69bb6fcce4fc13c645822e9ba82b4513990989
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-05-22 19:02:40 +0200

    Added option to connect to another api in purpose of filling database with data

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from CarApi import *
 
 try:
     print("Starting api...")
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='0.0.0.0', port=8000)
 except Exception as e:
     print(f"An error occurred while running Flask app: {e}")
